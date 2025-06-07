2fde7e6146f3d272038a2e47d062535a29dda7e6
minhphan6624 <quangminh6624@gmail.com>
2025-06-08 00:14:22 +1000

    fix image handling error (once again)

--- src/api/emailServices.js
+++ src/api/emailServices.js
@@ -4,7 +4,6 @@ import httpClient from './httpClient';
 export const sendEmail = async (data) => {
   try {
     const response = await httpClient.post('/email', data);
-    // Assuming backend response is { success: true, data: { ...createdUser } }
 
     return response.data;
   } catch (error) {
